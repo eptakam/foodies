4c4b1858ac157753716a923549320002ac39f99d
eptakam <emmataks@gmail.com>
2024-06-24 11:58:07 -0300

    Loading and rendering meal details via dynamic routes and routes parameters.

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,7 +1,33 @@
-export default function MealDetailsPage() {
+import Image from 'next/image';
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export default function MealDetailsPage({params}) {
+  // mealSlug : est le nom du dossier dans lequel se trouve le fichier page.js ([mealSlug]) et represente la cle de l'objet params. la valeur de cet objet est l'url actuelle
+  const meal = getMeal(params.mealSlug);
+
+  // bien formater les instructions pour les afficher dans le composant
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Meal Details</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+      {/* dangerouslySetInnerHTML : permet d'insérer du code HTML dans un composant React et nous expose à des attaques XSS (Cross-Site Scripting). */}
+        <p className={classes.instructions} dangerouslySetInnerHTML={{__html: meal.instructions,}}></p>
+      </main>
+    </>
   );
 }
