18fedefd109aa384a545cab11f5d0484f0e3d3c5
eptakam <emmataks@gmail.com>
2024-06-25 14:51:52 -0300

    working with server action responses and useFormState.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,18 @@
+'use client';
+
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
 import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+  /*
+    Note: 
+        useFormState() : permet de gérer l'état du formulaire. il a deux arguments dont le premier est un objet qui contient les valeurs initiales des champs du formulaire et le deuxième est l'etat initial du composant.
+  */
+  const [state, formAction] = useFormState(shareMeal, {message: null});
   
   return (
     <>
@@ -14,7 +23,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +52,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
